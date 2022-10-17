35923377a31024cfa428bc51793a8ad34cef3415
MayLinh <m.le@ll.lorentzlyceum.nl>
2022-10-17 14:09:10 +0200

    plaatjes converten, gepushed voor les

--- SkeereFlappyBird.py
+++ SkeereFlappyBird.py
@@ -5,22 +5,33 @@ import pygame
 pygame.init()
 
 # Popping the screen up.
-size = (600, 1000)
+size = (600, 800)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("May Linh's flappy bird")
 
 # Loop control.
 end = False
-#clock = pygame.time.Clock()
+# clock = pygame.time.Clock()
+
+# Converting the images
+background = pygame.image.load("images/background.png").convert()
+background = pygame.transform.scale(background, size)
+#                                    background.width * 1.481481481,
+#                                   background.height * 1.481481481)
+
+bird = pygame.image.load("images/bird_wingup.png")
+# bird = pygame.transform.scale(bird, )
 
 # Main program loop.
 while not end:
-
     # Closing the screen when the player presses the close button.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
 
-#    dt = clock.tick(20) / 1000
+    # Drawing code
+    screen.blit(background, (0, 0))
+    screen.blit(bird, (150, 300))
+
     pygame.display.update()
 pygame.quit()
