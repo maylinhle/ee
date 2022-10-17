95f15cd4a954a1f91eba7051fc5153a8fb8c3861
MayLinh <m.le@ll.lorentzlyceum.nl>
2022-10-17 15:35:53 +0200

    plaatjes converten, gepushed voor les

--- SkeereFlappyBird.py
+++ SkeereFlappyBird.py
@@ -4,6 +4,9 @@ import pygame
 
 pygame.init()
 
+# Variables
+gravity = 0.25
+
 # Popping the screen up.
 size = (600, 800)
 screen = pygame.display.set_mode(size)
@@ -15,12 +18,18 @@ end = False
 
 # Converting the images
 background = pygame.image.load("images/background.png").convert()
-background = pygame.transform.scale(background, size)
-#                                    background.width * 1.481481481,
-#                                   background.height * 1.481481481)
+#background = pygame.transform.scale(background, size)
+background_rect = background.get_rect()
+size_factor = size[1] / background_rect.height
+background = pygame.transform.scale(background,
+                                    (background_rect.width * size_factor,
+                                    background_rect.height * size_factor))
 
 bird = pygame.image.load("images/bird_wingup.png")
-# bird = pygame.transform.scale(bird, )
+bird = pygame.transform.scale(bird,(42, 30))
+
+ground = pygame.image.load("images/ground.png").convert()
+ground = pygame.transform.scale(ground, (600, 200))
 
 # Main program loop.
 while not end:
@@ -31,7 +40,8 @@ while not end:
 
     # Drawing code
     screen.blit(background, (0, 0))
-    screen.blit(bird, (150, 300))
+    screen.blit(bird, (150, 350))
+    screen.blit(ground, (0, 650))
 
     pygame.display.update()
 pygame.quit()
