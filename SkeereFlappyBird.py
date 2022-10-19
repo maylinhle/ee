125e7dcbda5bdd716ac60cdc54add8a4ab11cc09
MayLinh <m.le@ll.lorentzlyceum.nl>
2022-10-19 23:56:36 +0200

    ground laten bewegen voor poging om bird smooth te laten bewegen

--- SkeereFlappyBird.py
+++ SkeereFlappyBird.py
@@ -1,47 +1,75 @@
 # Imports
 import pygame
+import math
+import random
 
 
 pygame.init()
+clock = pygame.time.Clock()
 
-# Variables
-gravity = 0.25
+# Settings
+screenwidth = 600
+screenheight = 800
+
+gravity = 2
 
 # Popping the screen up.
-size = (600, 800)
+size = (screenwidth, screenheight)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("May Linh's flappy bird")
 
 # Loop control.
 end = False
-# clock = pygame.time.Clock()
 
 # Converting the images
 background = pygame.image.load("images/background.png").convert()
-#background = pygame.transform.scale(background, size)
-background_rect = background.get_rect()
-size_factor = size[1] / background_rect.height
-background = pygame.transform.scale(background,
-                                    (background_rect.width * size_factor,
-                                    background_rect.height * size_factor))
+background = pygame.transform.scale(background, size)
 
 bird = pygame.image.load("images/bird_wingup.png")
 bird = pygame.transform.scale(bird,(42, 30))
+birdRect = bird.get_rect(center=(100, 512))
 
 ground = pygame.image.load("images/ground.png").convert()
-ground = pygame.transform.scale(ground, (600, 200))
+ground = pygame.transform.scale(ground, (screenwidth, 200))
+
+# Positions images
+# Bird position
+birdX = 150
+birdY = 350
+birdUpdateTime = pygame.time.get_ticks()
+
+# Ground position
+groundX = 0
+groundY = 650
 
 # Main program loop.
 while not end:
     # Closing the screen when the player presses the close button.
+    beginTime = pygame.time.get_ticks()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                birdY -= 50
+
+    # update bird position
+    if beginTime - birdUpdateTime >= 25:
+        birdY += gravity
+        birdUpdateTime = beginTime
 
     # Drawing code
     screen.blit(background, (0, 0))
-    screen.blit(bird, (150, 350))
-    screen.blit(ground, (0, 650))
+    screen.blit(bird, (birdX, birdY))
+
+    #ground
+    screen.blit(ground, (groundX, groundY))
+    screen.blit(ground, (groundX + screenwidth, groundY))
+    groundX -= 1
+    if groundX <= -screenwidth:
+        groundX = 0
 
     pygame.display.update()
+    clock.tick(120)
 pygame.quit()
