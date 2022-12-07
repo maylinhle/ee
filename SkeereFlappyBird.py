e9d0257436e1f66eed4c270e59eb1f27604f46e4
MayLinh <m.le@ll.lorentzlyceum.nl>
2022-12-08 00:47:27 +0100

    LETSGO DE VOGEL BEWEEGT SMOOTH

--- SkeereFlappyBird.py
+++ SkeereFlappyBird.py
@@ -3,6 +3,11 @@ import pygame
 import random
 import sys
 
+def checkActive():
+    # Check if the bird doesn't hit the floor or flies too high (outside of the screen)
+    if birdPosition.top >= 700 or birdPosition.bottom <= 0:
+        return False
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -10,8 +15,9 @@ clock = pygame.time.Clock()
 screenwidth = 600
 screenheight = 800
 
-gravity = 0.25
+gravity = 0.30
 birdY = 0
+
 
 # Popping the screen up.
 size = (screenwidth, screenheight)
@@ -20,6 +26,7 @@ pygame.display.set_caption("May Linh's flappy bird")
 
 # Loop control.
 end = False
+gameActive = True
 
 # Converting the images
 # Background
@@ -44,20 +51,19 @@ birdPosition = bird.get_rect(center=(150, 350))
 
 # Ground position
 groundX = 0
-groundY = 650
+groundY = 700
 
 # Main program loop.
 while not end:
-    # Closing the screen when the player presses the close button.
-    #beginTime = pygame.time.get_ticks()
 
+    # Closing the screen when the player presses the close button.
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or gameActive == False:
             end = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 birdY = 0
-                birdY -= 12
+                birdY -= 10
 
     # update bird position
     #if beginTime - birdUpdateTime >= 25:
@@ -65,13 +71,16 @@ while not end:
     #    birdUpdateTime = beginTime
 
     # Drawing code
+    # Background
     screen.blit(background, (0, 0))
-
     # Bird
     birdY += gravity
     birdPosition.centery += birdY
     screen.blit(bird, birdPosition)
     #screen.blit(bird, (birdX, birdY))
+    # Colliding
+    gameActive = checkActive()
+
     # Ground
     screen.blit(ground, (groundX, groundY))
     screen.blit(ground, (groundX + screenwidth, groundY))
